d801ef7b8f6a1447c61dab1c15760433fc826f2a
Molly Waggett <mwaggett@alumni.reed.edu>
2015-09-21 13:50:56 -0700

    Create starting board.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-// var functionName = function(params) {
-//
-// };
+var createBoard = function() {
+  for (var row = 1; row <= 12; row++) {
+    $("#game tbody").append("<tr>");
+    for (var column = 1; column <= 12; column++) {
+      if (column === row) { // Condition for starting pattern
+        var status = 'live';
+      } else {
+        var status = 'dead';
+      }
+      $("#game tbody").append("<td class='"+status+"'></td>");
+    }
+    $("#game tbody").append("</tr>");
+  }
+};
 
-// $(document).ready(function() {
-//
-// });
+
+
+$(document).ready(function() {
+  createBoard();
+});
